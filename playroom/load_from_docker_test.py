87262fa70909329ff50ff20a59b248acb3d3aa73
Stephanie Chan <scychan@google.com>
2022-07-22 12:19:15 -0700

    Fixed import paths in playroom/load_from_docker_test.py
    PiperOrigin-RevId: 462677250
    Change-Id: I3c81c7c28f70b580c9b1413f36a8dcabadc90daf

--- playroom/load_from_docker_test.py
+++ playroom/load_from_docker_test.py
@@ -13,39 +13,39 @@
 # limitations under the License.
 # ==============================================================================
 
-"""Tests for dm_zipf_playroom.load_from_docker."""
+"""Tests for dm_zipf_playroom.zipfs_playroom.load_from_docker."""
 
 from absl import flags
 from absl.testing import absltest
 from absl.testing import parameterized
 from dm_env import test_utils
-import dm_zipf_playroom
 
-FLAGS = flags.FLAGS
+from zipfian_environments import playroom as zipfs_playroom
 
-flags.DEFINE_string(
+DOCKER_IMAGE_NAME = flags.DEFINE_string(
     'docker_image_name', None,
-    'Name of the Docker image that contains the Fast Language Learning Tasks. '
+    'Name of the Docker image that contains the Zipfian Distributions Tasks. '
     'If None, uses the default dm_zipf_playroom name')
 
 
 class LoadFromDockerTest(test_utils.EnvironmentTestMixin, absltest.TestCase):
 
   def make_object_under_test(self):
-    return dm_zipf_playroom.load_from_docker(
-        name=FLAGS.docker_image_name,
-        settings=dm_zipf_playroom.EnvironmentSettings(
+    return zipfs_playroom.load_from_docker(
+        name=DOCKER_IMAGE_NAME.value,
+        settings=zipfs_playroom.EnvironmentSettings(
             seed=123, level_name='lift/lift_shape_zipf2'))
 
 
-class FastMappingTaskTest(parameterized.TestCase):
+class ZipfianDistributionsTaskTest(parameterized.TestCase):
 
-  @parameterized.parameters(dm_zipf_playroom.FAST_MAPPING_TASK_LEVEL_NAMES)
+  @parameterized.parameters(
+      zipfs_playroom.ZIPFIAN_DISTRIBUTIONS_TASK_LEVEL_NAMES)
   def test_load_level(self, level_name):
     self.assertIsNotNone(
-        dm_zipf_playroom.load_from_docker(
-            name=FLAGS.docker_image_name,
-            settings=dm_zipf_playroom.EnvironmentSettings(
+        zipfs_playroom.load_from_docker(
+            name=DOCKER_IMAGE_NAME.value,
+            settings=zipfs_playroom.EnvironmentSettings(
                 seed=123, level_name=level_name)))
 
 
